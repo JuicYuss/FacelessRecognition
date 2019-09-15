2df00cb7021174a701c1efe40ae22b0cacd7ed50
Uslan Roz <37580970+yussiroz@users.noreply.github.com>
2019-09-15 15:17:07 +0300

    Update prep_model.py

--- prep_model.py
+++ prep_model.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Sep  3 14:19:33 2019
 
-@author: yussiroz, InbalWeiss
+@author: YussiRoz, InbalWeiss
+
 """
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.applications import NASNetMobile, VGG19
+from tensorflow.keras.layers import Flatten, Dense, Dropout, Conv2D
+from tensorflow.keras.models import Model
 import data
 from data import Images
 from common import resolve_single
@@ -14,7 +17,7 @@ from scipy.stats import mode
 """Upload super res GAN libs"""
 from model.srgan import generator
 from model.cnn import CNN
-from utils import tensor2numpy, shuffle, devide, create_onehot, per_label, devide_submission
+from utils import tensor2numpy, shuffle, devide, create_onehot, per_label, devide_submission, train_model
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
@@ -26,29 +29,72 @@ with Images('data/images.tar') as images:
     image = images._getitem(path)
     print ('read image {} of shape {}'.format(path, image.shape))
 
-"""Use SRGAN"""
-model = generator()
-model.load_weights('weights/srgan/gan_generator.h5')
 
 my_split=poses[0]
 my_split=[path[:-4] for path in my_split]
 
+
+"""Use SRGAN"""
+srgan = generator()
+srgan.load_weights('weights/srgan/gan_generator.h5')
+
+
 """Upload customed cnn model"""
 cnn = CNN(256, 256, 3, 101)
-cnn.load_weights('./cnn_weights.h5')
+cnn.load_weights('weights/custom/cnn_plus.h5')
+plot_model(cnn, to_file='./model.png', show_shapes=True, show_layer_names=True)
 
-filepath="./cnn_weights.h5"
-checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
+
+train_model(2, 'cnn_plus', cnn, srgan)
+
+#filepath="./cnn_weights.h5"
+#checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
+#callbacks_list = [checkpoint]
 
 """Prepare and train on a batch of data and labels, 10 iterations"""
-for i in range(10):
+for i in range(2):
     train_set = devide(24, 2, 2)
-    X = tensor2numpy('./data/', train_set, model)
+    X = tensor2numpy('./data/', train_set, srgan)
     x = [X[i] for i in X.keys()]
     train = np.array(x, dtype = "float64")
     y = create_onehot(X)
-    cnn.fit(train, y, batch_size=32, epochs=5, callbacks=callbacks_list)
+    history = cnn.fit(train, y, batch_size=32, epochs=5, callbacks=callbacks_list, validation_split=0.2)
+    # Plot training & validation accuracy values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+"""Upload, use transfer learning"""
+VGG=VGG19(input_shape=(224,224,3),include_top=False,weights='imagenet')
+#freeze NatNetMobile weights
+for layer in VGG.layers:
+    layer.trainable=False
+
+output=VGG.layers[6].output
+output=Flatten()(output)
+output=Dense(1024,activation='relu')(output)
+output=Dense(512,activation='relu')(output)
+output=Dropout(0.5)(output)
+output=Dense(256,activation='relu')(output)
+output=Dense(101,activation='softmax')(output)
+VGG=Model(VGG.input,output)
+VGG.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+plot_model(VGG, to_file='./model.png', show_shapes=True, show_layer_names=True)
+
+train_model(3, 'vgg', VGG, srgan, resize = True)
+
+"""Test, evaluate the model"""
+test_set = devide(12, 2, 2)
+X = tensor2numpy('./data/', test_set, srgan)
+x = [X[i] for i in X.keys()]
+test = np.array(x, dtype = "float64")
+y = create_onehot(X)
+evaluation = cnn.evaluate(test, y)
+prediction = cnn.predict(test)
 
 """Make predictions"""
 sss = devide_submission(5)
